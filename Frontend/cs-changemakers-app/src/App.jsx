c0b0ae7fc63fcccddc9092c83411f85b48fb93cf
El Hamid <jamesdav828@gmail.com>
2025-05-31 22:36:08 +0100

    Added Learner dashboard

--- Frontend/cs-changemakers-app/src/App.jsx
+++ Frontend/cs-changemakers-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Paymentform from './Components/Paymentform';
 import SignupPage from './Components/SignupPage';
+import LearnerDashboard from './Components/LearnerDashBoard';
 
 const App = () => {
   return (
-    <SignupPage />
+    <LearnerDashboard />
   )
 }
 
