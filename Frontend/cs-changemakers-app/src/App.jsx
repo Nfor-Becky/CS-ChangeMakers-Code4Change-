4560db22c1dff4239f1b6b0fb380ad000f1cb215
HRHBK <pamakia95@gmail.com>
2025-05-31 13:45:14 -0400

    Just started designing the users dashboard

--- Frontend/cs-changemakers-app/src/App.jsx
+++ Frontend/cs-changemakers-app/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Paymentform from './Components/Paymentform';
+import SendMoneyForm from './Components/SendMoneyForm';
+import UsersDashboard from './Components/UsersDashboard';
+
+import { Send } from '@mui/icons-material';
+
 
 const App = () => {
   return (
-    <Paymentform  />
+    <UsersDashboard/>
   )
 }
 
