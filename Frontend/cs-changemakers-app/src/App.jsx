4b876360ecc900ef3c4ed076fb139d4c4622cb9f
HRHBK <pamakia95@gmail.com>
2025-05-31 14:09:03 -0400

    Did some minor changes

--- Frontend/cs-changemakers-app/src/App.jsx
+++ Frontend/cs-changemakers-app/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Paymentform from './Components/Paymentform';
 import SendMoneyForm from './Components/SendMoneyForm';
 import UsersDashboard from './Components/UsersDashboard';
-
+import SignupPage from './Components/SignupPage';
 import { Send } from '@mui/icons-material';
 
 
 const App = () => {
   return (
-    <UsersDashboard/>
+    <SignupPage/>
   )
 }
 
